a171e62a0381883377df90733713657476af39a1
Leonardo Alves de Paula e Silva <leo@upwifi.com.br>
2019-08-20 17:13:21 -0300

    Adicionando materiais e aulas laboratório

---
+++ Laboratório/js/form.js
@@ -0,0 +1,12 @@
+jQuery(function ($) {
+    $("input#nome").val("dadsadasdadasdasdasdas");
+
+});
+
+jQuery(function ($) {
+    $("button#limpar").click(function($) {
+        $("#form_dados_pessoais").reset();
+
+    });
+});
+
